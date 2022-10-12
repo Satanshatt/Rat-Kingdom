76c5c28916cc5d99458d59c2cbdb9b6210f92a00
solunden <sofialundquist1@gmail.com>
2022-10-12 12:40:53 +0200

    player attribut plus getters

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -2,6 +2,49 @@
 
 public class Player {
 
-    //Sofia fia mamma MIA
-    
+    private int health;
+    private int mana;
+    private int strength;
+    private int dexterity;
+    private int intelligence;
+    private int xp;
+    private int level;
+
+    public Player() {
+        this.health = 100;
+        this.mana = 100;
+        this.strength = 10;
+        this.dexterity = 10;
+        this.intelligence = 10;
+        this.xp = 0;
+        this.level = 0;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getMana() {
+        return mana;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public int getDexterity() {
+        return dexterity;
+    }
+
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+    public int getXp() {
+        return xp;
+    }
+
+    public int getLevel() {
+        return level;
+    }
 }
