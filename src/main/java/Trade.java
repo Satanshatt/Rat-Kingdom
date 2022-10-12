f714c6c1224f5b697c0dea5185f29fa9b8792a7c
Moa Wieweg <moa@wieweg.se>
2022-10-12 15:17:54 +0200

    gjort trade interface

--- src/main/java/Trade.java
+++ src/main/java/Trade.java
@@ -1,17 +1,6 @@
 
 
-public abstract class Trade {
-    private String typeOfTrade;
-
-    public Trade(String typeOfTrade){
-        this.typeOfTrade = typeOfTrade;
-    }
-
-    public void setTypeOfTrade(String typeOfTrade){
-        this.typeOfTrade = typeOfTrade;
-    }
-
-    public String getTypeOfTrade(){
-        return typeOfTrade;
-    }
+interface Trade {
+    void raiseAttribute();
+    String getName();
 }
---
+++ src/main/java/builderTrade.java
@@ -0,0 +1,15 @@
+public class builderTrade implements Trade{
+    private String name = "builder";
+    private int addStartStrength = 2;
+
+    public builderTrade(String name, int addStartStrength){}
+
+    public void raiseAttribute(){
+
+    }
+
+    public String getName(){
+        return name;
+    }
+
+}
