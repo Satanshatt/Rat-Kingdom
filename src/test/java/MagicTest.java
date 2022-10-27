b291a6dd460ff5ebe1a24d2315a390d82bb55f23
Moa Wieweg <moa@wieweg.se>
2022-10-27 14:40:29 +0200

    magic test

---
+++ src/test/java/MagicTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class MagicTest {
+    Player player;
+
+    @BeforeEach
+    void setup(){
+        player = new BasicPlayer();
+    }
+
+    @Test
+    public void testFire(){
+        //Magic fire = new Fire(player);
+
+    }
+
+    @Test
+    public void testIce(){
+        //Magic ice = new Ice(player);
+    }
+}
