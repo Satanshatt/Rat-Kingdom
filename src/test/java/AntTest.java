f0c39a59056365943a73f91f27c81f60b2382a9d
Yrsa <yrsa@trail.gg>
2022-11-10 19:00:22 +0100

    Uppdaterat Ant och AntTest

--- src/test/java/AntTest.java
+++ src/test/java/AntTest.java
@@ -5,9 +5,29 @@ import org.mockito.Mockito;
 
 public class AntTest {
 
+    int NINETYFIVE = 95;
+
     @Test
-    public void antMissingAttackTest() {
+    public void Should_BeFalse_When_NPCAttacksAndMissesPlayer() {
+        Ant ant = new Ant(1, 2);
+        Player player = new Player();
+        player.setPosX(5);
+        player.setPosY(2);
+        assertFalse(ant.isPlayerWithinReach(player));
 
     }
+
+    @Test
+    public void Should_BeEqual_When_NPCAttacksAndHitsPlayer(){
+        Ant ant = new Ant(1, 2);
+        Player player = new Player();
+        player.setPosX(2);
+        player.setPosY(2);
+        ant.setDamage(5);
+        int playerExpectedHealth = NINETYFIVE;
+        ant.attack(player);
+        assertEquals(playerExpectedHealth, player.getHealth());
+    }
+
 
 }
